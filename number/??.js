a961dd3eb69527458e4078910de2d3436e0c84b1
bb <bohdan.buchok@ukma.edu.ua>
2023-10-29 11:05:31 +0200

    fixup number

---
+++ number/??.js
@@ -0,0 +1,5 @@
+console.log(null ?? 0);
+console.log(undefined ?? 0);
+console.log("not null or undefined" ?? 0);
+console.log(43 ?? 0);
+console.log(0.00000000000000000000000000000000000000000001 ?? 0);
---
+++ number/Number(str).js
@@ -0,0 +1,7 @@
+console.log(Number("23434"));
+console.log(Number("  23434        "));
+
+//!
+console.log(Number("  2  3434        "));
+console.log(Number("  2.3434        "));
+console.log(Number("  2,3434        "));
