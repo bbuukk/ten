a9927e6fb353268532b3d2f4bcad06998b50d7a6
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-03-28 06:58:50 +0200

    String.prototype.search() & Array.prototype.indexOf()

--- string/search.js
+++ string/search.js
@@ -1,35 +1,27 @@
-// String.prototype.search()
+import { greenANSI, redANSI, terminator, yellowANSI } from "../variables.mjs";
 
-// The search() method of String values executes a search for a match between a regular expression and this string, returning the index of the first match in the string
+const args = [
+  { a: "string", b: "t" },
+  { a: "stringundefined", b: "undefined" },
+  { a: "stringundefined", b: undefined },
+  /*
+  Here’s the relevant part of the ECMAScript specification that explains this behavior (Section 21.2.3.2.2):
 
-const paragraph =
-  "The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?";
+    If pattern is undefined, let P be the empty string; otherwise, let P be ? ToString(pattern).
 
-// Any character that is not a word character or whitespace
-const regex = /[^\w\s]/g;
+*/
+  { a: "stringnull", b: null },
+];
 
-console.log(paragraph.search(regex));
-// Expected output: 43
+args.forEach(({ a, b, c, d }, index) => {
+  const searched = a.search(b);
 
-console.log(paragraph[paragraph.search(regex)]);
-// Expected output: "."
+  console.log(terminator);
+  console.log(searched);
+});
+console.log(terminator);
 
-//!
-// Parameters
+// const regex = new RegExp(undefined);
+// console.log("🚀 ~ regex:", regex);
 
-// regexp
-
-//     A regular expression object, or any object that has a Symbol.search method.
-
-//     If regexp is not a RegExp object and does not have a Symbol.search method, it is implicitly converted to a RegExp by using new RegExp(regexp).
-
-// !
-// The g flag of regexp has no effect on the search() result, and the search always happens as if the regex's lastIndex is 0. For more information on the behavior of search(), see RegExp.prototype[@@search]().
-
-//examples
-
-const str = "hey JudE";
-const re = /[A-Z]/;
-const reDot = /[.]/;
-console.log(str.search(re)); // returns 4, which is the index of the first capital letter "J"
-console.log(str.search(reDot)); // returns -1 cannot find '.' dot punctuation
+// console.log("helloundefined".search(undefined));
